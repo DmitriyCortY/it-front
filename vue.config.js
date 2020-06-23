afedd2b337083dbf25dcec569c51b028159aa138
ujenik <ujenik@totonis.com>
2020-06-23 18:58:25 +0300

    vue config

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,8 @@ module.exports = {
     "transpileDependencies": [
         "vuetify"
     ],
+    publicPath: '/wp-content/themes/totonis/constructor/front/dist/',
+    chainWebpack: config => {
+        config.plugins.delete('prefetch');
+    }
 }
